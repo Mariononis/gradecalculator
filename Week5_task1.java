abd959ee9bede0a8db94874f5288b49c09cad942
Mario Nonis <83999136+Mariononis@users.noreply.github.com>
2025-04-08 05:42:56 +0530

    Update Week5_task1.java

--- Week5_task1.java
+++ Week5_task1.java
@@ -25,7 +25,7 @@ public class Week5_task1 {
             System.out.println("Fail");
         }
         else {
-            System.out.println("Invalid Value enter");
+            System.out.println("Invalid Value entered");
             input.close();
         }
     }
